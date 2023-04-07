1c4f6a9bccf7632604d3903a36c4447df1b31a2b
Karolis Liutackas <59737461+karolisliutack@users.noreply.github.com>
2023-04-07 10:06:17 +0300

    Update kubernetesclusterroles.tf

--- kubernetesclusterroles.tf
+++ kubernetesclusterroles.tf
@@ -21,3 +21,45 @@ resource "kubernetes_cluster_role" "readonly" {
     verbs      = ["get", "list", "watch"]
   }
 }
+
+resource "kubernetes_role_binding" "admin_binding" {
+  metadata {
+    name = "admin-binding"
+  }
+
+  role_ref {
+    kind = "ClusterRole"
+    name = kubernetes_cluster_role.admin.metadata[0].name
+    api_group = "rbac.authorization.k8s.io"
+
+  }
+
+  subject {
+    kind = "User"
+    name = "admin@example.com"
+  }
+    depends_on = [
+    kubernetes_cluster_role.admin,
+  ]
+}
+
+resource "kubernetes_role_binding" "readonly_binding" {
+  metadata {
+    name = "readonly-binding"
+  }
+
+  role_ref {
+    kind = "ClusterRole"
+    name = kubernetes_cluster_role.readonly.metadata[0].name
+    api_group = "rbac.authorization.k8s.io"
+
+  }
+
+  subject {
+    kind = "User"
+    name = "readonly@example.com"
+  }
+    depends_on = [
+    kubernetes_cluster_role.readonly,
+  ]
+}
